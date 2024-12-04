a8b318ab01d706e35e3e09d2a4f0f486be872031
vmhoafng <vominhhoang044@gmail.com>
2024-12-04 20:11:57 +0700

    upd

--- app/(client)/booking/page.tsx
+++ app/(client)/booking/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import { PassengerInfo } from "../../components/PassengerInfo";
-import { PaymentSummary } from "../../components/PaymentSummary";
 import { Button } from "../../components/Button";
-import { PromoCodeInput } from "../../components/PromoCodeInput";
 import { useSeatsContext } from "@/app/context/SeatsContext";
 import Seats from "@/app/components/Seats";
 import { useRouter } from "next/router";
@@ -19,11 +16,8 @@ export default function BookingPage({ params }: BookingPageProps) {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
           <Seats trainId={id} />
-          <PassengerInfo />
-          <PromoCodeInput />
         </div>
         <div>
-          <PaymentSummary />
           <Button className="w-full mt-6">Xác nhận</Button>
         </div>
       </div>

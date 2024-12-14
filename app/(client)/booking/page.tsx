94845bb9ccc351ec068d360d399636c269a1ee65
Diggory <chuongle.nt1@gmail.com>
2024-12-14 12:17:21 +0700

    updatebooking

--- app/(client)/booking/page.tsx
+++
@@ -1,9 +0,0 @@
-import BookingForm from './pages/BookingForm'
-
-export default function BookingPage() {
-    return (
-        <div className="container-custom mx-auto py-8">
-            <BookingForm />
-        </div>
-    )
-}
